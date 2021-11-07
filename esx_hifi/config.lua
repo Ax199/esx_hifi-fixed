943f87cca6e6b6cffe04e73fc960e35f80fb82b8
Ax <77904626+Ax199@users.noreply.github.com>
2021-11-06 23:10:20 -0300

    Update config.lua

--- esx_hifi/config.lua
+++ esx_hifi/config.lua
@@ -1,10 +1,9 @@
---SCRIPT FIXEADO POR alvaro199#0054
 Config = {}
 Config.Locale = 'es'
 Config.distance = 15
 
 Config["translations"] = {
-    tooClose = "~r~Too close to another speaker!",
+    tooClose = "Demasiado cerca de otro altavoz!",
     pickUp = "Presiona [E] para usar y [G] para recoger",
   }
   
