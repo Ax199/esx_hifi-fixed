b9becfd340d07842649d0c8db4ce5ada5c3f2a96
Ax <77904626+Ax199@users.noreply.github.com>
2021-11-06 23:08:51 -0300

    Update fr.lua

--- esx_hifi/locales/fr.lua
+++ esx_hifi/locales/fr.lua
@@ -1,13 +1,10 @@
 Locales ['es'] = {
-    ['put_hifi'] = 'Acabas de soltar la BoomBox',
---	['get_hifi'] = 'Pick up the BoomBox',
-	['play_music'] = 'Iniciar la musica',
-	['stop_music'] = 'Parar la musica',
-	['volume_music'] = 'Ajustar el volumen',
-	['hifi_alreadyOne'] = 'Ya tienes un BoomBox contigo',
-	['set_volume'] = 'Ingrese el nivel de volumen (entre 0 y 100)',
-	['play_id'] = 'Ingrese la identificación del video de YouTube',
-	['sound_limit'] = 'The volume must be between 0 and 100',
---	['hifi_help'] = 'presione ~ INPUT_CONTEXT ~ to adjust the music'
+    ['put_hifi'] = 'Vous venez de déposer la BoomBox',
+	['play_music'] = 'Démarrer la musique',
+	['stop_music'] = 'Arrête la musique',
+	['volume_music'] = 'Ajuster le volume',
+	['hifi_alreadyOne'] = 'Vous avez déjà une BoomBox avec vous',
+	['set_volume'] = 'Entrez le niveau de volume (entre 0 et 100)',
+	['play_id'] = 'Saisissez lidentifiant de la vidéo YouTube',
+	['sound_limit'] = 'Le volume doit être compris entre 0 et 100',
 }
-
