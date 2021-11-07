f8cc7aa5f41485a1c62dfc409ac8ab89513b74d3
Ax <77904626+Ax199@users.noreply.github.com>
2021-11-06 22:56:35 -0300

    Add files via upload

---
+++ esx_hifi/__resource.lua
@@ -0,0 +1,32 @@
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+
+description 'ESX Hifi'
+
+version '1.0.0'
+
+server_scripts {
+    '@es_extended/locale.lua',
+    'locales/es.lua',
+    'server/main.lua',
+    'server/m-main.lua',
+    'config.lua'
+}
+
+client_script {
+    '@es_extended/locale.lua',
+    'client/main.lua',
+    'client/m-main.lua',
+    'locales/es.lua',
+    'config.lua'
+}
+
+ui_page('html/index.html')
+
+files {
+    'html/index.html',
+    'html/app.js'
+}
+
+dependencies {
+    'es_extended'
+}
---
+++ esx_hifi/config.lua
@@ -0,0 +1,13 @@
+--SCRIPT FIXEADO POR alvaro199#0054
+Config = {}
+Config.Locale = 'es'
+Config.distance = 15
+
+Config["translations"] = {
+    tooClose = "~r~Too close to another speaker!",
+    pickUp = "Presiona [E] para usar y [G] para recoger",
+  }
+  
+  Config.Enable3DText = true
+  Config.UsingESX = true
+  Config.EnableCommand = true 
