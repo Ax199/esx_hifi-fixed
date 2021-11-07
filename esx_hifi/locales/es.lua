8cab7e2948013721c6fcde045c4ced47e4228007
Ax <77904626+Ax199@users.noreply.github.com>
2021-11-06 23:06:13 -0300

    Update es.lua

--- esx_hifi/locales/es.lua
+++ esx_hifi/locales/es.lua
@@ -1,6 +1,5 @@
 Locales ['es'] = {
     ['put_hifi'] = 'Acabas de soltar la BoomBox',
---	['get_hifi'] = 'Pick up the BoomBox',
 	['play_music'] = 'Iniciar la musica',
 	['stop_music'] = 'Parar la musica',
 	['volume_music'] = 'Ajustar el volumen',
@@ -8,6 +7,4 @@ Locales ['es'] = {
 	['set_volume'] = 'Ingrese el nivel de volumen (entre 0 y 100)',
 	['play_id'] = 'Ingrese la identificación del video de YouTube',
 	['sound_limit'] = 'The volume must be between 0 and 100',
---	['hifi_help'] = 'presione ~ INPUT_CONTEXT ~ to adjust the music'
 }
-
