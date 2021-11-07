419d71f2f5e3777755ef9bf9ff275e224a038014
Ax <77904626+Ax199@users.noreply.github.com>
2021-11-07 00:56:34 -0300

    Update config.lua

--- esx_hifi/config.lua
+++ esx_hifi/config.lua
@@ -1,12 +1,13 @@
 Config = {}
 Config.Locale = 'es'
 Config.distance = 15
+Config.Enable3DText = true
+Config.UsingESX = true
+Config.EnableCommand = true 
 
 Config["translations"] = {
     tooClose = "Demasiado cerca de otro altavoz!",
     pickUp = "Presiona [E] para usar y [G] para recoger",
   }
   
- Config.Enable3DText = true
- Config.UsingESX = true
- Config.EnableCommand = true 
+
