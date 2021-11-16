b1c3149a739b9ec59cc24d76df02ab4b573b11d5
Ax <77904626+Ax199@users.noreply.github.com>
2021-11-16 09:47:01 -0300

    Update config.lua

--- esx_hifi/config.lua
+++ esx_hifi/config.lua
@@ -5,7 +5,6 @@ Config.Enable3DText = true
 Config.UsingESX = true
 Config.EnableCommand = true 
 
-
 Config["translations"] = {
     tooClose = "~r~Too close to another speaker!",
     pickUp = "Press [E] to use and [G] to pick up",
